8bf0720620ce5a60c6c891d90f5e550233c1c521
andbilous-digis <andbilous@gmail.com>
2020-04-25 14:30:01 +0300

    Updates

---
+++ precache-manifest.97ce64f88461e492922473c1055d8fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb704bc66e0557636a6d3c1317c1174",
+    "url": "/cursor-redux/index.html"
+  },
+  {
+    "revision": "6118aa10fd3a6bef7566",
+    "url": "/cursor-redux/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "42625d6a519c9bfbb104",
+    "url": "/cursor-redux/static/js/2.6b71087f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cursor-redux/static/js/2.6b71087f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6118aa10fd3a6bef7566",
+    "url": "/cursor-redux/static/js/main.f4afe925.chunk.js"
+  },
+  {
+    "revision": "558647b5e4a20cffd559",
+    "url": "/cursor-redux/static/js/runtime-main.b630e50e.js"
+  }
+]);
